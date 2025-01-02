ca0af47c25ae0d230896372e6df6db6ed7476e5d
jii1n <yj5596901@gmail.com>
2025-01-02 10:49:46 +0900

    Add files via upload

---
+++ package_install.R
@@ -0,0 +1,32 @@
+# script to install R packages
+
+library(readxl)
+
+# 1. using CRAN
+install.packages('readxl')
+
+# 2. install from local file
+install.packages('/Users/kr/Desktop/demo/basicR/stringr_1.5.0.tar.gz', repos = NULL)
+library(stringr)
+
+# 3. install packages from github (public repositories)
+# https://github.com/rstudio/shiny
+library(shiny)
+install.packages('remotes')
+library(remotes)
+
+remotes::install_github('rstudio/shiny')
+library(shiny)
+
+# 4. install from bioconductor
+# we need a package called BiocManager which is available on CRAN
+library(Biostrings)
+install.packages('BiocManager')
+library(BiocManager)
+BiocManager::install('Biostrings')
+
+# where are my packages saved
+.libPaths()
+
+#how to remove packages?
+remove.packages()
